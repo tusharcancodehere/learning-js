bcff7a1d45cca386773c484c4b41ddda3051acdc
tushar <thetusharverma2505@gmail.com>
2026-08-27 11:43:35 +0530

    huehuehuehue

---
+++ 01_variables.js
@@ -0,0 +1,7 @@
+const accountId = '123456';
+let accountEmail = 'user@example.com';
+var accountPassword = 'securepassword123';
+console.log(`Account ID: ${accountId}`);
+console.log(`Account Email: ${accountEmail}`);
+console.log(`Account Password: ${accountPassword}`);
+console.table({ accountId, accountEmail, accountPassword });
