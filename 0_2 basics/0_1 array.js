d475ceedbe0ad252bbf4168eb03a44eaca14221e
tushar <thetusharverma2505@gmail.com>
2026-08-27 22:44:52 +0530

    Array

---
+++ 0_2 basics/0_1 array.js
@@ -0,0 +1,16 @@
+// arrays
+const arr = [0,1, 2, 3, 4, 5];
+const myHeroes = ['Superman', 'Batman', 'Wonder Woman', 'Flash'];
+
+const Myarray = new Array(1, 2, 3, 4);
+console.log(Myarray[1]);
+
+// array methods
+arr.push(6); // adds to the end of the array
+console.log("after push: ", arr);
+arr.pop(); // removes the last element of the array
+console.log("after pop: ", arr);
+arr.unshift(9); // adds to the beginning of the array
+console.log("after unshift: ", arr);
+arr.shift(); // removes the first element of the array
+console.log("after shift: ", arr);
